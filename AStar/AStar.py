7bb2896d612512ad009ed9399c06fac369c9466e
Alvaro Felix <alfelix@ucm.es>
2025-03-28 12:35:23 +0100

    Resolucion de TODOs

--- AStar/AStar.py
+++ AStar/AStar.py
@@ -10,6 +10,7 @@ class AStar:
         self.problem = problem #problema a resolver
 
     def GetPlan(self):
+        """
         findGoal = False
         #TODO implementar el algoritmo A*
         #cosas a tener en cuenta:
@@ -24,13 +25,64 @@ class AStar:
         path = []
         #mientras no encontremos la meta y haya elementos en open....
         #TODO implementar el bucle de búsqueda del algoritmo A*
+        return path"""
+
+        findGoal = False
+        self.open.clear()
+        self.precessed.clear()
+        self.open.append(self.problem.Initial())  #Metemos el nodo inicial en la lista open
+        path = []
+
+        #Mientras no encontremos la meta y haya elementos en open...
+        while not findGoal and len(self.open) > 0:
+            #Ordenamos open por el valor F (G + H) del nodo (menor primero)
+            self.open.sort(key=lambda node: node.F())
+            
+            #Extraemos el nodo con menor F (el primero en la lista)
+            current = self.open.pop(0)
+
+            #Metemos el nodo actual al MAPA close
+            self.precessed.add(current)
+
+            #Comprobamos si hemos alcanzado el objetivo
+            if self.problem.IsGoal(current):
+                findGoal = True
+                path = self.ReconstructPath(current)  #Reconstruimos el path
+
+            else:
+                #Expandimos los sucesores del nodo actual
+                successors = self.problem.GetSuccessors(current)
+
+                if len(successors) == 0:
+                    #Si no existen sucesores, no existe una solucion apartir de aqui
+                    #Devolvemos el path vacio
+                    continue
+
+                for successor in successors:
+                    if successor in self.precessed:
+                        continue  #Ignoramos nodos ya procesados
+
+                    successorInOpen = self.GetSucesorInOpen(successor)
+                    newG = current.G() + self.problem.Cost(current, successor)
+
+                    if successorInOpen is None:
+                        #Si no esta en open, lo configuramos y lo metemos en open
+                        self._ConfigureNode(successor, current, newG)
+                        self.open.append(successor)
+                    else:
+                        # Si ya esta en open, comprobamos si el nuevo camino es mas eficiente
+                        if newG < successorInOpen.G():
+                            self._ConfigureNode(successorInOpen, current, newG)
+
         return path
 
     #nos permite configurar un nodo (node) con el padre y la nueva G
     def _ConfigureNode(self, node, parent, newG):
+        #TODO REALIZADO
         node.SetParent(parent)
         node.SetG(newG)
-        #TODO Setearle la heuristica que está implementada en el problema. (si ya la tenía será la misma pero por si reutilizais este método para otras cosas)
+        # Seteamos la heurística usando el método del problema.
+        node.SetH(self.problem.Heuristic(node))
 
     #nos dice si un sucesor está en abierta. Si esta es que ya ha sido expandido y tendrá un coste, comprobar que le nuevo camino no es más eficiente
     #En caso de serlos, _ConfigureNode para setearle el nuevo padre y el nuevo G, asi como su heurística
@@ -47,9 +99,16 @@ class AStar:
 
     #reconstruye el path desde la meta encontrada.
     def ReconstructPath(self, goal):
+        #TODO REALIZADO
+        #Reconstruimos el camino desde el nodo objetivo hasta el nodo inicial
         path = []
-        #TODO: devuelve el path invertido desde la meta hasta que el padre sea None.
-        return path
+        current = goal
+
+        while current is not None:  #Mientras no lleguemos al nodo inicial (padre=None)
+            path.append(current)
+            current = current.GetParent()
+
+        return path[::-1]  #Invertimos el camino para devolverlo en orden correcto
 
 
 
---
+++ AStar/SibNode.py
@@ -0,0 +1,12 @@
+#Creamos una clase hija como se nos pide en Node.py
+class SibNode(Node):
+    def __init__(self, parent, g, state):
+        super().__init__(parent, g)
+        self.state = state  #Estado especifico del nodo
+    
+    def IsEqual(self, node):
+        #Verificar si el nodo comparado es del mismo tipo
+        if not isinstance(node, SibNode):
+            return False
+        #Comparar el estado almacenado en ambos nodos
+        return self.state == node.state
